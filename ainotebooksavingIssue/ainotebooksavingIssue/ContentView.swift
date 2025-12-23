333c23ff6f99f4bb7d8cba9a066a92cb3188f704
Abdullah Noor <abdullahnoor.edu1@gmail.com>
2025-12-22 17:54:03 -0800

    updated dashboard+ designing + rendering + auto saving

---
+++ ainotebooksavingIssue/ainotebooksavingIssue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ainotebooksavingIssue
+//
+//  Created by Abdullah Noor on 12/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ainotebooksavingIssue/ainotebooksavingIssue/ainotebooksavingIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ainotebooksavingIssueApp.swift
+//  ainotebooksavingIssue
+//
+//  Created by Abdullah Noor on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct ainotebooksavingIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
